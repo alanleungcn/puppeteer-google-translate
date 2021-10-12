7104faf3d9a74c7972ed54dc41dbbc5cb86e6a23
Rifan M Fauzi <rifan.refun@gmail.com>
2021-10-13 05:03:19 +0700

    combine all translated texts

--- src/translate/text.ts
+++ src/translate/text.ts
@@ -6,16 +6,29 @@ export default async (
 	opt: PuppeteerOptions
 ): Promise<string> => {
 	const { browser, page, timeout } = await launch(opt);
-	try {
-		await page.goto('https://translate.google.com/' + query);
-		const el = await page.waitForSelector('span>span>span[jsaction]', {
-			timeout
-		});
-		const result = await el.evaluate((e) => e.textContent);
-		return result;
-	} catch (err) {
-		throw err;
-	} finally {
-		await browser.close();
-	}
+    try {
+        await page.goto('https://translate.google.com/' + query);
+        const el1 = await page.waitForSelector('span>span[data-phrase-index="0"]>span[jsaction]', {
+            timeout
+        });
+        const texts = [await el1.evaluate((e) => e.textContent)];
+
+        const counter = (await page.$$('span>span[data-phrase-index]>span[jsaction]')).length;
+        if (counter > 1) {
+            for (let i=1; i<counter; i++) {
+                const elx = await page.waitForSelector('span>span[data-phrase-index="'+i+'"]>span[jsaction]', {
+                    timeout
+                });
+                texts.push(await elx.evaluate((e) => e.textContent));
+            }
+        }
+
+        return texts.join(' ');
+    }
+    catch (err) {
+        throw err;
+    }
+    finally {
+        await browser.close();
+    }
 };
