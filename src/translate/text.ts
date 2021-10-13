878c5c226468675d5e04c7c4b5e075372ec8c2a5
Alan Leung <39870851+alanleungcn@users.noreply.github.com>
2021-10-13 11:46:27 +0800

    Update text.ts

--- src/translate/text.ts
+++ src/translate/text.ts
@@ -2,33 +2,35 @@ import launch from '../launch';
 import { PuppeteerOptions } from '../types';
 
 export default async (
-	query: string,
-	opt: PuppeteerOptions
+  query: string,
+  opt: PuppeteerOptions
 ): Promise<string> => {
-	const { browser, page, timeout } = await launch(opt);
-    try {
-        await page.goto('https://translate.google.com/' + query);
-        const el1 = await page.waitForSelector('span>span[data-phrase-index="0"]>span[jsaction]', {
-            timeout
-        });
-        const texts = [await el1.evaluate((e) => e.textContent)];
-
-        const counter = (await page.$$('span>span[data-phrase-index]>span[jsaction]')).length;
-        if (counter > 1) {
-            for (let i=1; i<counter; i++) {
-                const elx = await page.waitForSelector('span>span[data-phrase-index="'+i+'"]>span[jsaction]', {
-                    timeout
-                });
-                texts.push(await elx.evaluate((e) => e.textContent));
-            }
+  const { browser, page, timeout } = await launch(opt);
+  try {
+    await page.goto('https://translate.google.com/' + query);
+    const el1 = await page.waitForSelector(
+      'span>span[data-phrase-index="0"]>span[jsaction]',
+      {
+        timeout,
+      }
+    );
+    const texts = [await el1.evaluate((e) => e.textContent)];
+    const len = (await page.$$('span>span[data-phrase-index]>span[jsaction]'))
+      .length;
+    if (len === 1) return texts[0];
+    for (let i = 1; i < len; i++) {
+      const el = await page.waitForSelector(
+        `span>span[data-phrase-index="${i}"]>span[jsaction]`,
+        {
+          timeout,
         }
-
-        return texts.join(' ');
+      );
+      texts.push(await el.evaluate((e) => e.textContent));
     }
-    catch (err) {
-        throw err;
-    }
-    finally {
-        await browser.close();
-    }
+    return texts.join(' ');
+  } catch (err) {
+    throw err;
+  } finally {
+    await browser.close();
+  }
 };
